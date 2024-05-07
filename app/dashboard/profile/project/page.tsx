b319e47f80fc165140e166d5c17321fe02617307
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-05-07 20:42:33 +0530

    Profile -project section

--- app/dashboard/profile/project/page.tsx
+++ app/dashboard/profile/project/page.tsx
@@ -1,17 +1,214 @@
+"use client";
+import React, { useState } from "react";
+import { MuiChipsInput } from "mui-chips-input";
 import ProfileNav from "@/components/ProfileNav";
-import ProjectPart from "@/components/ProjectPart";
-
-import React from "react";
 
 const SocialPage = () => {
+  // Interface for typescript for type check
+  interface Project {
+    id: number;
+    imageUrl: string;
+    techStack: string[];
+    projectName: string;
+    description: string;
+    projectLink: string;
+  }
+  // UseState for state management of Project
+  const [projects, setProjects] = useState<Project[]>([
+    {
+      id: 1,
+      imageUrl: "",
+      techStack: [],
+      projectName: "",
+      description: "",
+      projectLink: "",
+    },
+  ]);
+
+  // Current Page State Management
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const projectsPerPage = 1; // Change this value to adjust the number of projects per page
+
+  // Handle Input  Change  Function
+  const handleInputChange = (
+    id: number,
+    name: keyof Project,
+    value: string | string[]
+  ) => {
+    const newProjects = projects.map((project) =>
+      project.id === id ? { ...project, [name]: value } : project
+    );
+    setProjects(newProjects);
+  };
+  // Handle Add Project Function
+  const handleAddProject = () => {
+    const newId = projects.length + 1;
+    setProjects([
+      ...projects,
+      {
+        id: newId,
+        imageUrl: "",
+        techStack: [],
+        projectName: "",
+        description: "",
+        projectLink: "",
+      },
+    ]);
+    setCurrentPage(Math.ceil(newId / projectsPerPage));
+  };
+
+  const indexOfLastProject = currentPage * projectsPerPage;
+  const indexOfFirstProject = indexOfLastProject - projectsPerPage;
+  const currentProjects = projects.slice(
+    indexOfFirstProject,
+    indexOfLastProject
+  );
+
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber);
   return (
     <div className="w-4/5 ">
       <ProfileNav />
       <hr />
-      <h5 className="text-2xl font-bold my-5 mx-10">Project</h5>
+      <div className="flex flex-row justify-between">
+        <h5 className="text-2xl font-bold my-5 mx-10">Project</h5>
+        <button className="bg-black text-white justify-between my-4 mx-10 px-4 rounded-3xl ">
+          save changes
+        </button>
+      </div>
+
       <div className="overflow-y-auto h-[550px]">
         <div className="w-1/2 mx-5 my-10">
-          <ProjectPart />
+          <div>
+            {currentProjects.map((project) => (
+              <div key={project.id}>
+                {/* Image Url Input */}
+                <div className="my-1">
+                  <h1 className="my-1">Image Link</h1>
+                  <input
+                    type="text"
+                    name="imageUrl"
+                    value={project.imageUrl}
+                    onChange={(event) =>
+                      handleInputChange(
+                        project.id,
+                        "imageUrl",
+                        event.target.value
+                      )
+                    }
+                    placeholder="Add Image Url"
+                    className="border-black border-[1px] rounded-md w-[100%] py-1 px-3"
+                  />
+                </div>
+
+                {/* Tech Stack */}
+                <div className="my-1">
+                  <h1 className="my-1">Tech Stack</h1>
+                  <MuiChipsInput
+                    value={project.techStack}
+                    onChange={(chips) =>
+                      handleInputChange(project.id, "techStack", chips)
+                    }
+                    className="rounded-md w-full  "
+                  />
+
+                  {/**<input
+              type="text"
+              name="techStack"
+              value={project.techStack}
+              onChange={(event) =>
+                handleInputChange(project.id, "techStack", event.target.value)
+              }
+              placeholder="Tech Stack"
+              className="border-black border-[1px] rounded-md w-[100%] py-1 px-3"
+            /> */}
+                </div>
+
+                {/* Name Of the Project */}
+                <div className="my-1">
+                  <h1 className="my-1">Project Name</h1>
+                  <input
+                    type="text"
+                    name="projectName"
+                    value={project.projectName}
+                    onChange={(event) =>
+                      handleInputChange(
+                        project.id,
+                        "projectName",
+                        event.target.value
+                      )
+                    }
+                    placeholder="Project Name"
+                    className="border-black border-[1px] rounded-md w-[100%] py-1 px-3"
+                  />
+                </div>
+                {/* Description of the project */}
+                <div className="my-1">
+                  <h1 className="my-1">Description</h1>
+                  <input
+                    type="text"
+                    name="description"
+                    value={project.description}
+                    onChange={(event) =>
+                      handleInputChange(
+                        project.id,
+                        "description",
+                        event.target.value
+                      )
+                    }
+                    placeholder="Description"
+                    className="border-black border-[1px] rounded-md w-[100%] py-1 px-3"
+                  />
+                </div>
+
+                {/* Link Of the Project */}
+                <div className="my-1">
+                  <h1 className="my-1">Project Link</h1>
+                  <input
+                    type="text"
+                    name="projectLink"
+                    value={project.projectLink}
+                    onChange={(event) =>
+                      handleInputChange(
+                        project.id,
+                        "projectLink",
+                        event.target.value
+                      )
+                    }
+                    placeholder="Project Link"
+                    className="border-black border-[1px] rounded-md w-[100%] py-1 px-3"
+                  />
+                </div>
+                <hr className="my-3" />
+              </div>
+            ))}
+
+            <div className=" flex flex-row">
+              {/* Pagination buttons */}
+              {projects.length > projectsPerPage && (
+                <div>
+                  {Array.from(
+                    { length: Math.ceil(projects.length / projectsPerPage) },
+                    (_, index) => index + 1
+                  ).map((pageNumber) => (
+                    <button
+                      key={pageNumber}
+                      className="mx-3 my-2 px-2 rounded-full border-black border-[1px] border-dashed"
+                      onClick={() => paginate(pageNumber)}
+                    >
+                      {pageNumber}
+                    </button>
+                  ))}
+                </div>
+              )}
+              <button
+                className=" mx-3 my-2 px-2 rounded-full border-black border-[1px] border-dashed"
+                onClick={handleAddProject}
+              >
+                +
+              </button>
+            </div>
+            {/*<button onClick={() => console.log(projects)}>Show Console</button> */}
+          </div>
           <hr className="my-3" />
         </div>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
 const Home = () => {
-  return <main>Homepage</main>;
+  return (
+    <main className="text-center">
+      <div className="text-4xl font-semibold my-40">
+        Currently <br /> You are in homepage
+      </div>
+      <div className="bg-black text-white text-4xl font-semibold my-40 py-20 mx-96 rounded-full">
+        <Link href={"/dashboard/home"}>Press Me</Link>
+      </div>
+    </main>
+  );
 };
 export default Home;
